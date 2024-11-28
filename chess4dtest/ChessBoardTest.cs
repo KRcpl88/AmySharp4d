dece0b3c0b9a879123f9905cd56f8152e0c4331c
krcpl88 <krcpl@hotmail.com>
2024-11-28 14:25:29 -0800

    fix chess board unit test

--- chess4dtest/ChessBoardTest.cs
+++ chess4dtest/ChessBoardTest.cs
@@ -12,10 +12,10 @@ namespace tgreiner.amy.chess.engine.Tests
         public void fenTest()
         {
             //var board = new ChessBoard("////////////////////////////rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq -");
-            var board = new ChessBoard("/////////////////////////////3r4/pr1Pk1p1/8/7P/6P1/3R3K/5R2 w - -");
+            var board = new ChessBoard("///////////////////////////////2k2K///r6R w - -");
 
-            Assert.IsTrue(board.getMask(true, ChessConstants_Fields.PAWN).countBits() == 8) ;
-            Assert.IsTrue(board.getMask(false, ChessConstants_Fields.PAWN).countBits() == 8) ;
+            Assert.IsTrue(board.getMask(true, ChessConstants_Fields.ROOK).countBits() == 1) ;
+            Assert.IsTrue(board.getMask(false, ChessConstants_Fields.ROOK).countBits() == 1) ;
         }
     }
 }
