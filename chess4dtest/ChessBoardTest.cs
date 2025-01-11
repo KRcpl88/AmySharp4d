b4e2f0d2f0d86af6e73698ee8ef73a6bc7117140
krcpl88 <krcpl@hotmail.com>
2025-01-11 15:18:44 -0800

    Add bishop attack tests

--- chess4dtest/ChessBoardTest.cs
+++ chess4dtest/ChessBoardTest.cs
@@ -163,41 +163,6 @@ namespace tgreiner.amy.chess.engine.Tests
         }
 
 /*
-  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
-8| K |   |   |   |   |   |   |   |
-  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
-7  |   |   |   |   |   |   |   |
-    \ /c\ /d\ /e\ /f\ /g\ /h\ /
-6    |   |   |   |   |   |   |
-      \ /d\ /e\ /f\ /g\ /h\ /
-5      |   |   |   |   |   |
-        \ /e\ /f\ /g\ /h\ /
-4        |   |   |   |   |
-          \ /f\ /g\ /h\ /
-3          |   |   |   |
-            \ /g\ /h\ /
-2            |   |   |
-              \ /h\ /
-1              |   |
-h               \ / 
-
-    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
-8  |   |   |   |   |   |   |   |
-  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
-7|   |   |   |   |   |   |   |   |
-  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
-6  |   |   |   |   |   |   |   |
-    \ /c\ /d\ /e\ /f\ /g\ /h\ /
-5    |   |   |   |   |   |   |
-      \ /d\ /e\ /f\ /g\ /h\ /
-4      |   |   |   |   |   |
-        \ /e\ /f\ /g\ /h\ /
-3        |   |   |   |   |
-          \ /f\ /g\ /h\ /
-2          |   |   |   |
-            \ /g\ /h\ /
-1            |   |   |
-g             \ / \ / 
 
       /a\ /b\ /c\ /d\ /e\ /f\ 
 8    |   |   |   |   |   |   |
@@ -289,24 +254,6 @@ c     \ / \ / \ / \ / \ / \ /
 1  |   |   |   |   |   |   |   |
 b   \ / \ / \ / \ / \ / \ / \ / 
 
-                /a\ 
-8              |   |
-              /a\ /b\ 
-7            |   |   |
-            /a\ /b\ /c\ 
-6          |   |   |   |
-          /a\ /b\ /c\ /d\ 
-5        |   |   |   |   |
-        /a\ /b\ /c\ /d\ /e\ 
-4      |   |   |   |   |   |
-      /a\ /b\ /c\ /d\ /e\ /f\ 
-3    |   |   |   |   |   |   |
-    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
-2  |   |   |   |   |   |   |   |
-  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
-1|   |   |   |   |   |   |   |*K*|
-a \ / \ / \ / \ / \ / \ / \ / \ / 
-
 */
         [TestMethod()]
         public void knightAttackTests()
@@ -336,7 +283,241 @@ a \ / \ / \ / \ / \ / \ / \ / \ /
             String knightAttcks = board.ToStringHex(squareKnight);
 
             BitBoard moves = board.getAttackTo(squareKnight);
-            moves.SetBit(BoardConstants_Fields.HG6);
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,4,5))) ==1); //fe6
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,5,4))) ==1); //ff5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,5,2))) ==1); //ff3
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,4,2))) ==1); //fe3
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,2,4))) ==1); //fc5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,2,5))) ==1); //fc6
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,4,5))) ==1); //ee6
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,5,4))) ==1); //ef5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,5,1))) ==1); //ef2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,4,1))) ==1); //ee2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,1,4))) ==1); //eb5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,1,5))) ==1); //eb6
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,1,5))) ==1); //cb6
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,2,5))) ==1); //cc6
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,5,2))) ==1); //cf3
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,5,1))) ==1); //cf2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,2,1))) ==1); //cc2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,1,2))) ==1); //cb3
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,1,4))) ==1); //bb5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,2,4))) ==1); //bc5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,4,2))) ==1); //be3
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,4,1))) ==1); //be2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,2,1))) ==1); //bc2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,1,4))) ==1); //bb3
+        }
+
+
+/*
+
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+8| K |   |   |   |   |   |   |. .|
+  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
+7  |   |   |   |   |   |   |   |
+    \ /c\ /d\ /e\ /f\ /g\ /h\ /
+6    |   |   |   |   |   |   |
+      \ /d\ /e\ /f\ /g\ /h\ /
+5      |   |   |   |   |   |
+        \ /e\ /f\ /g\ /h\ /
+4        |   |   |   |   |
+          \ /f\ /g\ /h\ /
+3          |   |   |   |
+            \ /g\ /h\ /
+2            |   |   |
+              \ /h\ /
+1              |   |
+h               \ / 
+
+    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
+8  |   |   |   |   |   |   |   |
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+7|. .|   |   |   |   |   |. .|   |
+  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
+6  |   |   |   |   |   |   |   |
+    \ /c\ /d\ /e\ /f\ /g\ /h\ /
+5    |   |   |   |   |   |   |
+      \ /d\ /e\ /f\ /g\ /h\ /
+4      |   |   |   |   |   |
+        \ /e\ /f\ /g\ /h\ /
+3        |   |   |   |   |
+          \ /f\ /g\ /h\ /
+2          |   |   |   |
+            \ /g\ /h\ /
+1            |. .|   |
+g             \ / \ / 
+
+      /a\ /b\ /c\ /d\ /e\ /f\ 
+8    |   |   |   |   |   |   |
+    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
+7  |   |   |   |   |   |   |   |
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+6|   |. .|   |   |   |. .|   |   |
+  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
+5  |   |   |   |   |   |   |   |
+    \ /c\ /d\ /e\ /f\ /g\ /h\ /
+4    |   |   |   |   |   |   |
+      \ /d\ /e\ /f\ /g\ /h\ /
+3      |   |   |   |   |   |
+        \ /e\ /f\ /g\ /h\ /
+2        |   |. .|   |   |
+          \ /f\ /g\ /h\ /
+1          |   |   |   |
+f           \ / \ / \ / 
+
+        /a\ /b\ /c\ /d\ /e\ 
+8      |   |   |   |   |   |
+      /a\ /b\ /c\ /d\ /e\ /f\ 
+7    |   |   |   |   |   |   |
+    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
+6  |   |   |   |   |   |   |   |
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+5|   |   |. .|   |. .|   |   |   |  Hashkey: d2f2c33345d86a3
+  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
+4  |   |   |   |   |   |   |   |
+    \ /c\ /d\ /e\ /f\ /g\ /h\ /
+3    |   |   |. .|   |   |   |
+      \ /d\ /e\ /f\ /g\ /h\ /
+2      |   |   |   |   |   |
+        \ /e\ /f\ /g\ /h\ /
+1        |   |   |   |   | *
+e         \ / \ / \ / \ / 
+
+          /a\ /b\ /c\ /d\ 
+8        |   |   |   |   |
+        /a\ /b\ /c\ /d\ /e\ 
+7      |   |   |   |   |   |
+      /a\ /b\ /c\ /d\ /e\ /f\ 
+6    |   |   |   |   |   |   |
+    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
+5  |   |   |   |   |   |   |   |
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+4|   |   |   | B |   |   |   |   |
+  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
+3  |   |   |   |   |   |   |   |
+    \ /c\ /d\ /e\ /f\ /g\ /h\ /
+2    |   |   |   |   |   |   |
+      \ /d\ /e\ /f\ /g\ /h\ /
+1      |   |   |   |   |   |
+d       \ / \ / \ / \ / \ / 
+
+            /a\ /b\ /c\ 
+8          |   |   |   |
+          /a\ /b\ /c\ /d\ 
+7        |   |   |   |   |
+        /a\ /b\ /c\ /d\ /e\ 
+6      |   |   |   |   |   |
+      /a\ /b\ /c\ /d\ /e\ /f\ 
+5    |   |   |. .|   |   |   |
+    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
+4  |   |   |   |   |   |   |   |
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+3|   |   |. .|   |. .|   |   |   |
+  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
+2  |   |   |   |   |   |   |   |
+    \ /c\ /d\ /e\ /f\ /g\ /h\ /
+1    |   |   |   |   |   |   |
+c     \ / \ / \ / \ / \ / \ / 
+
+              /a\ /b\ 
+8            |   |   |
+            /a\ /b\ /c\ 
+7          |   |   |   |
+          /a\ /b\ /c\ /d\ 
+6        |   |. .|   |   |
+        /a\ /b\ /c\ /d\ /e\ 
+5      |   |   |   |   |   |
+      /a\ /b\ /c\ /d\ /e\ /f\ 
+4    |   |   |   |   |   |   |
+    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
+3  |   |   |   |   |   |   |   |
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+2|   |. .|   |   |   |. .|   |   |
+  \ /b\ /c\ /d\ /e\ /f\ /g\ /h\ /
+1  |   |   |   |   |   |   |   |
+b   \ / \ / \ / \ / \ / \ / \ / 
+
+                /a\ 
+8              |   |
+              /a\ /b\ 
+7            |. .|   |
+            /a\ /b\ /c\ 
+6          |   |   |   |
+          /a\ /b\ /c\ /d\ 
+5        |   |   |   |   |
+        /a\ /b\ /c\ /d\ /e\ 
+4      |   |   |   |   |   |
+      /a\ /b\ /c\ /d\ /e\ /f\ 
+3    |   |   |   |   |   |   |
+    /a\ /b\ /c\ /d\ /e\ /f\ /g\ 
+2  |   |   |   |   |   |   |   |
+  /a\ /b\ /c\ /d\ /e\ /f\ /g\ /h\ 
+1|. .|   |   |   |   |   |. .|*K*|
+a \ / \ / \ / \ / \ / \ / \ / \ / 
+
+
+
+*/
+
+        [TestMethod()]
+        public void bishopAttackTests()
+        {
+            var board = new ChessBoard("1/2/2/3/3/3/4/4/4/4/5/5/5/5/5/6/6/6/6/6/6/7/7/7/7/7/7/7/K/8/8/8/3B/8/8/7k w - -");
+
+            BitBoard pieces = board.getMask(true, ChessConstants_Fields.KING);
+            Assert.IsTrue(pieces.countBits() == 1);
+            int squareKing = pieces.findFirstOne();
+            Lfr lfrKing = (Lfr) squareKing;
+            Assert.IsTrue(squareKing == BoardConstants_Fields.HA8);
+
+            pieces = board.getMask(false, ChessConstants_Fields.KING);
+            Assert.IsTrue(pieces.countBits() == 1);
+            squareKing = pieces.findFirstOne();
+            lfrKing = (Lfr) squareKing;
+            Assert.IsTrue(squareKing == BoardConstants_Fields.HH1);
+
+            pieces = board.getMask(true, ChessConstants_Fields.BISHOP);
+            Assert.IsTrue(pieces.countBits() == 1);
+
+
+            int squareBishop = pieces.findFirstOne();
+            Lfr lrfKnight = (Lfr) squareBishop;
+            Assert.IsTrue(squareBishop == BoardConstants_Fields.HD4);
+
+            String bishopAttcks = board.ToStringHex(squareBishop);
+
+            BitBoard moves = board.getAttackTo(squareBishop);
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(7,7,7))) ==1); //hh8
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(6,6,6))) ==1); //gg7
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(6,6,0))) ==1); //gg1
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(6,0,6))) ==1); //ga7
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,5,5))) ==1); //ff6
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,5,1))) ==1); //ff2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(5,1,5))) ==1); //fb6
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,4,4))) ==1); //ee5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,4,2))) ==1); //ee3
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(4,2,4))) ==1); //ec5
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,2,4))) ==1); //cc5
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,4,2))) ==1); //ce3
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(2,2,4))) ==1); //cc3
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,1,5))) ==1); //bb6
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,5,1))) ==1); //bf2
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(1,1,1))) ==1); //bb2
+
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(0,0,6))) ==1); //aa7
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(0,6,0))) ==1); //ag1
+            Assert.IsTrue(moves.GetBit((int)(Lfr)(new HexLfr(0,0,0))) ==1); //aa1
 
         }
 
